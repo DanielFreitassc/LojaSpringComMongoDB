dcb10c3aa9339bba6ebca470d68200d5d7be994a
DanielFreitassc <danielfreitassc@gmail.com>
2024-05-17 18:18:33 -0300

    Refactor: arrumando cards

--- Telemedicina/src/components/cards/index.jsx
+++ Telemedicina/src/components/cards/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import './style.css';
 
 const Cards = ({ data }) => {
     return (
         <div className="card">
-            <img src={data.icon} alt="icone" className="card-icon" />
-            <h1 className="card-title">{data.title}</h1>
+            <div className='icon-c'><img src={data.icon} alt="icone" className="card-icon" /></div>
+            <span className="card-title">{data.title}</span>
             <p className="card-body">{data.body}</p>
         </div>
     );
